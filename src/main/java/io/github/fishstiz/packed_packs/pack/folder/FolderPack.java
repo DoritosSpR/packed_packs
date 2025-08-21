2a19ca702f630cd2df0f76ec027e1b900d9afa5c
fishstiz <fishstickz7777@gmail.com>
2025-08-21 18:49:22 +0800

    fix datapacks from additional folders enabling automatically

--- src/main/java/io/github/fishstiz/packed_packs/pack/folder/FolderPack.java
+++ src/main/java/io/github/fishstiz/packed_packs/pack/folder/FolderPack.java
@@ -2,15 +2,14 @@ package io.github.fishstiz.packed_packs.pack.folder;
 
 import io.github.fishstiz.packed_packs.config.ConfigLoader;
 import io.github.fishstiz.packed_packs.config.Folder;
+import io.github.fishstiz.packed_packs.pack.PackAssets;
 import io.github.fishstiz.packed_packs.transform.interfaces.IPack;
 import io.github.fishstiz.packed_packs.util.ResourceUtil;
 import io.github.fishstiz.packed_packs.util.lang.ObjectsUtil;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.packs.*;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackCompatibility;
-import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.world.flag.FeatureFlagSet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -26,29 +25,20 @@ import java.util.concurrent.CompletableFuture;
 public class FolderPack extends Pack implements IPack {
     public static final Component FOLDER_OPEN_TEXT = ResourceUtil.getText("folder.open");
     public static final Component FOLDER_DESCRIPTION = ResourceUtil.getText("folder");
-    public static final PackSource FOLDER_SOURCE = PackSource.create(
-            name -> Component.translatable(
-                            "pack.nameAndSource",
-                            name,
-                            ResourceUtil.getModName().withStyle(ChatFormatting.YELLOW)
-                    )
-                    .withStyle(ChatFormatting.GRAY),
-            false
-    );
     public static final PackSelectionConfig FOLDER_SELECTION_CONFIG = new PackSelectionConfig(false, Position.TOP, false);
     public static final Metadata FOLDER_METADATA = new Metadata(FOLDER_DESCRIPTION, PackCompatibility.COMPATIBLE, FeatureFlagSet.of(), Collections.emptyList());
     private final Path path;
     private final String additionalPrefx;
 
-    public FolderPack(String id, String name, String additionalPrefx, Path path) {
+    public FolderPack(String id, String name, String additionalPrefix, Path path) {
         super(
-                new PackLocationInfo(id, Component.literal(name), FOLDER_SOURCE, Optional.empty()),
+                new PackLocationInfo(id, Component.literal(name), PackAssets.SOURCE, Optional.empty()),
                 new FolderResourcesSupplier(path),
                 FOLDER_METADATA,
                 FOLDER_SELECTION_CONFIG
         );
         this.path = path;
-        this.additionalPrefx = additionalPrefx;
+        this.additionalPrefx = additionalPrefix;
     }
 
     public CompletableFuture<Folder> loadConfig() {
