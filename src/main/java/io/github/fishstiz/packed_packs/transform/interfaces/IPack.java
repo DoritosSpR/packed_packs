c6e0790d1f19329ac339089716038897405196d3
fishstiz <fishstickz7777@gmail.com>
2025-08-21 14:37:13 +0800

    additional pack folders (#18)

--- src/main/java/io/github/fishstiz/packed_packs/transform/interfaces/IPack.java
+++ src/main/java/io/github/fishstiz/packed_packs/transform/interfaces/IPack.java
@@ -1,5 +1,6 @@
 package io.github.fishstiz.packed_packs.transform.interfaces;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.nio.file.Path;
@@ -17,5 +18,12 @@ public interface IPack {
 
     default @Nullable Path packed_packs$getPath() {
         return null;
+    }
+
+    default void packed_packs$setAdditionalPrefix(@Nullable String additionalPrefix) {
+    }
+
+    default @NotNull String packed_packs$getAdditionalPrefix() {
+        return "";
     }
 }
