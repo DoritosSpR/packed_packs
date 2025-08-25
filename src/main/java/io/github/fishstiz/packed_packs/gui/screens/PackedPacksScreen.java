f46c1f0821616ba76fa280f92fe31bacf6a86bf9
fishstiz <fishstickz7777@gmail.com>
2025-08-26 05:23:05 +0800

    drag on deez

--- src/main/java/io/github/fishstiz/packed_packs/gui/screens/PackedPacksScreen.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/screens/PackedPacksScreen.java
@@ -624,6 +624,7 @@ public class PackedPacksScreen extends PackListEventHandler implements Toggleabl
 
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        this.setDragged(null);
         if (isRightClick(button) && !this.options.isMouseOver(mouseX, mouseY)) {
             this.openContextMenu((int) mouseX, (int) mouseY);
             return true;
