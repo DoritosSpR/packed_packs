1a19c7f704538abe0d58c7a5745f2d034658ebff
fishstiz <fishstickz7777@gmail.com>
2025-09-01 15:27:34 +0800

    properly replace original screen (#23)

--- src/main/java/io/github/fishstiz/packed_packs/gui/metadata/PackSelectionScreenArgs.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/metadata/PackSelectionScreenArgs.java
@@ -1,8 +1,11 @@
 package io.github.fishstiz.packed_packs.gui.metadata;
 
+import io.github.fishstiz.packed_packs.transform.mixin.PackSelectionModelAccessor;
 import io.github.fishstiz.packed_packs.transform.mixin.PackSelectionScreenAccessor;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.packs.PackSelectionScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.PackRepository;
 
 import java.nio.file.Path;
@@ -14,6 +17,23 @@ public record PackSelectionScreenArgs(
         Path packDir,
         Component title
 ) {
+    public static PackSelectionScreenArgs extract(PackSelectionScreen screen) {
+        PackSelectionScreenAccessor packScreen = (PackSelectionScreenAccessor) screen;
+        PackSelectionModelAccessor model = (PackSelectionModelAccessor) packScreen.getModel();
+        return new PackSelectionScreenArgs(
+                model.packed_packs$getRepository(),
+                model.packed_packs$getOutput(),
+                packScreen.packed_packs$getPackDir(),
+                screen.getTitle()
+        );
+    }
+
+    public PackType packType() {
+        return this.repository == Minecraft.getInstance().getResourcePackRepository()
+                ? PackType.CLIENT_RESOURCES
+                : PackType.SERVER_DATA;
+    }
+
     public PackSelectionScreen createScreen() {
         return new PackSelectionScreen(this.repository, this.output, this.packDir, this.title);
     }
