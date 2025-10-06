296dbb0321410ed7c6456e00bad04e86dea2265f
fishstiz <fishstickz7777@gmail.com>
2025-10-07 01:37:58 +0800

    improve context menu rendering

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/contextmenu/MenuItem.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/contextmenu/MenuItem.java
@@ -1,15 +1,50 @@
 package io.github.fishstiz.fidgetz.gui.components.contextmenu;
 
 import com.google.common.util.concurrent.Runnables;
+import io.github.fishstiz.fidgetz.gui.renderables.RenderableRect;
+import io.github.fishstiz.fidgetz.gui.renderables.sprites.Sprite;
 import io.github.fishstiz.packed_packs.util.constants.Theme;
 import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
 
 public interface MenuItem {
-    MenuItem SEPARATOR = new SimpleMenuItem(Component.empty(), Runnables.doNothing());
+    MenuItem SEPARATOR = new MenuItem() {
+        @Override
+        public Component text() {
+            return Component.empty();
+        }
+
+        @Override
+        public Runnable action() {
+            return Runnables.doNothing();
+        }
+
+        @Override
+        public boolean shouldAutoSeparate() {
+            return false;
+        }
+
+        @Override
+        public boolean active() {
+            return false;
+        }
+    };
 
     Component text();
 
     Runnable action();
+
+    default @Nullable RenderableRect background() {
+        return null;
+    }
+
+    default @Nullable Sprite icon() {
+        return null;
+    }
+
+    default boolean shouldAutoSeparate() {
+        return true;
+    }
 
     default boolean active() {
         return true;
