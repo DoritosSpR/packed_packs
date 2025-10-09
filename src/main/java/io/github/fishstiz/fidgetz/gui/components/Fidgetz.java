399dbe68a7ef5a7bbacd1e095d13860e73e11f40
fishstiz <fishstickz7777@gmail.com>
2025-10-09 12:34:56 +0800

    fix overflow area of context menu capturing clicks

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/Fidgetz.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/Fidgetz.java
@@ -2,23 +2,19 @@ package io.github.fishstiz.fidgetz.gui.components;
 
 import io.github.fishstiz.fidgetz.gui.shapes.GuiRectangle;
 import io.github.fishstiz.fidgetz.util.GuiUtil;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.layouts.LayoutElement;
 import net.minecraft.client.gui.navigation.ScreenRectangle;
 import org.jetbrains.annotations.NotNull;
 
 public interface Fidgetz extends GuiEventListener, GuiRectangle, LayoutElement {
-    private boolean isUncovered(double mouseX, double mouseY) {
-        if (Minecraft.getInstance().screen instanceof ToggleableDialogContainer dialogContainer) {
-            return !dialogContainer.isChildCoveredAtPoint(this, mouseX, mouseY);
-        }
-        return true;
+    default boolean isHovered(double mouseX, double mouseY) {
+        return GuiUtil.isHovered(this, mouseX, mouseY);
     }
 
     @Override
     default boolean isMouseOver(double mouseX, double mouseY) {
-        return GuiUtil.containsPoint(this, mouseX, mouseY) && this.isUncovered(mouseX, mouseY);
+        return this.containsPoint(mouseX, mouseY);
     }
 
     @Override
