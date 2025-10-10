32f53b47e245c2283e6bee29741cf5fe75450a57
fishstiz <fishstickz7777@gmail.com>
2025-10-11 02:03:51 +0800

    sealed PackListEvent

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FileRenameEvent.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FileRenameEvent.java
@@ -4,9 +4,5 @@ import io.github.fishstiz.packed_packs.gui.components.pack.PackList;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.packs.repository.Pack;
 
-public record FileRenameEvent(PackList target, Pack renamed, Component newName) implements PackListEvent {
-    @Override
-    public boolean pushToHistory() {
-        return false;
-    }
+public record FileRenameEvent(PackList target, Pack renamed, Component newName) implements FileEvent {
 }
--- src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FolderOpenEvent.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FolderOpenEvent.java
@@ -3,9 +3,5 @@ package io.github.fishstiz.packed_packs.gui.components.events;
 import io.github.fishstiz.packed_packs.gui.components.pack.PackList;
 import io.github.fishstiz.packed_packs.pack.folder.FolderPack;
 
-public record FolderOpenEvent(PackList target, FolderPack opened) implements PackListEvent {
-    @Override
-    public boolean pushToHistory() {
-        return false;
-    }
+public record FolderOpenEvent(PackList target, FolderPack opened) implements FileEvent {
 }
