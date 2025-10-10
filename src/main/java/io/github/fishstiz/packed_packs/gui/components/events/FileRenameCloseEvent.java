f502cfeff1588274e32da97a00d0aca097273a2a
fishstiz <fishstickz7777@gmail.com>
2025-10-11 02:03:51 +0800

    sealed PackListEvent

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FileRenameCloseEvent.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FileRenameCloseEvent.java
@@ -3,9 +3,5 @@ package io.github.fishstiz.packed_packs.gui.components.events;
 import io.github.fishstiz.packed_packs.gui.components.pack.PackList;
 import net.minecraft.server.packs.repository.Pack;
 
-public record FileRenameCloseEvent(PackList target, Pack trigger) implements PackListEvent {
-    @Override
-    public boolean pushToHistory() {
-        return false;
-    }
+public record FileRenameCloseEvent(PackList target, Pack trigger) implements FileEvent {
 }
