4e3802860b079d3076100769aee15252397ff8ef
fishstiz <fishstickz7777@gmail.com>
2025-10-12 15:35:27 +0800

    optimize file ops

--- src/main/java/io/github/fishstiz/packed_packs/pack/PackFileOperations.java
+++ src/main/java/io/github/fishstiz/packed_packs/pack/PackFileOperations.java
@@ -1,10 +1,16 @@
 package io.github.fishstiz.packed_packs.pack;
 
+import io.github.fishstiz.packed_packs.config.Folder;
+import io.github.fishstiz.packed_packs.pack.folder.FolderPack;
+import io.github.fishstiz.packed_packs.pack.folder.FolderResources;
 import io.github.fishstiz.packed_packs.transform.interfaces.FilePack;
 import io.github.fishstiz.packed_packs.util.PackUtil;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.server.packs.repository.Pack;
 
 import java.nio.file.Path;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class PackFileOperations {
     private final PackOptionsContext options;
@@ -24,13 +30,50 @@ public class PackFileOperations {
                ((FilePack) pack).packed_packs$getPath() != null;
     }
 
+    // I wouldn't have had to do this if the nested pack ids weren't modified,
+    // but reverting this would be a breaking change. Oops.
+    // If a folder pack is renamed outside here, the config won't be remapped.
+    private void remapFolderConfig(FolderPack folderPack, String name, Path path) {
+        Folder folder = this.repository.getFolderConfig(folderPack);
+        if (folder == null) return;
+
+        String newId = PackUtil.generatePackId(name);
+        List<String> newPackIds = new ObjectArrayList<>();
+
+        String pattern = "^" + Pattern.quote(folderPack.getId());
+        for (String nestedPackId : folder.getPackIds()) {
+            if (nestedPackId.startsWith(folderPack.getId())) {
+                String newPackId = nestedPackId.replaceAll(pattern, newId);
+                newPackIds.add(newPackId);
+            } else {
+                newPackIds.add(nestedPackId);
+            }
+        }
+
+        folder.trySetPackIds(newPackIds);
+        folder.save(path.resolve(FolderResources.FOLDER_CONFIG_FILENAME));
+    }
+
     public boolean renamePack(Pack pack, String name) {
         if (!this.isOperable(pack)) {
             return false;
         }
 
         Path path = PackUtil.validatePackPath(pack);
-        return path != null && PackUtil.renamePath(path, path.getParent().resolve(name));
+        if (path == null) {
+            return false;
+        }
+
+        Path newPath = path.getParent().resolve(name);
+        if (!PackUtil.renamePath(path, newPath)) {
+            return false;
+        }
+
+        if (pack instanceof FolderPack folderPack) {
+            this.remapFolderConfig(folderPack, name, newPath);
+        }
+
+        return true;
     }
 
     public boolean deletePack(Pack pack) {
