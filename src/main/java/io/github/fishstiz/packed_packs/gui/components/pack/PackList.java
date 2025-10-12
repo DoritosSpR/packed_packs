7e35a9239898939669fd8f5ed9980304b0ad7a6a
fishstiz <fishstickz7777@gmail.com>
2025-10-12 20:23:57 +0800

    dev menu event handling

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/PackList.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/PackList.java
@@ -585,8 +585,12 @@ public abstract class PackList extends AbstractDynamicList<PackList.Entry> imple
                                 .build()
                 ));
             }
-            this.devMenu = devMode ? new PackListDevMenu(PackList.this.options, this.context, this::onRequire) : null;
+            this.devMenu = devMode ? createDevMenu(PackList.this.options, this.context) : null;
             ModAdditions.addToEntry(PackList.this.options.getConfig().packType(), this);
+        }
+
+        protected @Nullable PackListDevMenu createDevMenu(PackOptionsContext options, SelectionContext<Pack> context) {
+            return new PackListDevMenu(options, context);
         }
 
         public Pack pack() {
@@ -652,9 +656,6 @@ public abstract class PackList extends AbstractDynamicList<PackList.Entry> imple
             }
 
             return this.sendSelection();
-        }
-
-        protected void onRequire(Pack trigger, List<Pack> requiredPacks) {
         }
 
         protected boolean handleMouseAction(MouseSelectionHandler.Action action) {
