8e0c70a62f75212989701979c9c2d07baa2e8437
fishstiz <fishstickz7777@gmail.com>
2025-10-13 00:20:29 +0800

    minor stuff

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/SelectionContext.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/SelectionContext.java
@@ -1,13 +1,8 @@
 package io.github.fishstiz.packed_packs.gui.components;
 
-import java.util.Collections;
 import java.util.List;
 
 public record SelectionContext<T>(List<T> selection, T item) {
-    public SelectionContext {
-        selection = Collections.unmodifiableList(selection);
-    }
-
     public boolean isSelected() {
         return this.selection.contains(this.item);
     }
