3d6ddf3f102426cb08d030936dd8ffb9da327f99
fishstiz <fishstickz7777@gmail.com>
2025-10-14 06:08:42 +0800

    improve events and history

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/events/SelectionEvent.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/events/SelectionEvent.java
@@ -1,15 +1,8 @@
 package io.github.fishstiz.packed_packs.gui.components.events;
 
 import io.github.fishstiz.packed_packs.gui.components.pack.PackList;
-import net.minecraft.server.packs.repository.Pack;
 
-import java.util.List;
-
-public record SelectionEvent(PackList target, List<Pack> selected) implements PackListEvent {
-    public SelectionEvent(PackList target) {
-        this(target, target.copySelection());
-    }
-
+public record SelectionEvent(PackList target) implements PackListEvent {
     @Override
     public boolean pushToHistory() {
         return true;
