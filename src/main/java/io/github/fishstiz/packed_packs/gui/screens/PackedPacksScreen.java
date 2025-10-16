fd07ea59256ac6a6ecd7d11562c4d302bc8e3a70
fishstiz <fishstickz7777@gmail.com>
2025-10-16 20:06:47 +0800

    do not capture event name (unfinished)

--- src/main/java/io/github/fishstiz/packed_packs/gui/screens/PackedPacksScreen.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/screens/PackedPacksScreen.java
@@ -649,7 +649,7 @@ public class PackedPacksScreen extends PackListEventHandler implements
         }
 
         if (this.isUnlocked() && event.pushToHistory() && notFolderDialogEvent) {
-            this.history.push(this.captureState(event.name()));
+            this.history.push(this.captureState());
         }
     }
 
