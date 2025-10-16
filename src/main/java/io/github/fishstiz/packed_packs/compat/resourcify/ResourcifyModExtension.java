510161e2a0d1f270f369431fe844ff89a36a0ca4
fishstiz <fishstickz7777@gmail.com>
2025-10-16 13:42:28 +0800

    small mod ext optimization

--- src/main/java/io/github/fishstiz/packed_packs/compat/resourcify/ResourcifyModExtension.java
+++ src/main/java/io/github/fishstiz/packed_packs/compat/resourcify/ResourcifyModExtension.java
@@ -2,7 +2,7 @@ package io.github.fishstiz.packed_packs.compat.resourcify;
 
 import io.github.fishstiz.fidgetz.gui.layouts.FlexLayout;
 import io.github.fishstiz.packed_packs.compat.Mod;
-import io.github.fishstiz.packed_packs.compat.api.ModExtension;
+import io.github.fishstiz.packed_packs.compat.ModExtensionInternal;
 import io.github.fishstiz.packed_packs.gui.screens.PackedPacksScreen;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.packs.PackSelectionScreen;
@@ -12,10 +12,10 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class ResourcifyModExtension implements ModExtension {
+public class ResourcifyModExtension implements ModExtensionInternal {
     @Override
-    public ResourceLocation id() {
-        return Mod.RESOURCIFY.getInternalId();
+    public Mod mod() {
+        return Mod.RESOURCIFY;
     }
 
     @Override
@@ -25,7 +25,7 @@ public class ResourcifyModExtension implements ModExtension {
 
     @Override
     public void onCreateHeader(PackType type, FlexLayout header, PackedPacksScreen screen, PackSelectionScreen original) {
-        Mod.RESOURCIFY.wrapError(header, original, screen.getTitle(), (layout, packScreen, title) -> {
+        this.mod().wrapError(header, original, screen.getTitle(), (layout, packScreen, title) -> {
             List<? extends Button> buttons = ResourcifyButtons.getButtons(packScreen, title);
             if (buttons != null) {
                 for (Button button : buttons.reversed()) {
