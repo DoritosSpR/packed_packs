aab4a343c17d47d0a7573d71554096d095f7be4b
fishstiz <fishstickz7777@gmail.com>
2025-10-27 03:43:35 +0800

    fix resourcify buttons not appearing

--- src/main/java/io/github/fishstiz/packed_packs/compat/resourcify/ResourcifyButtons.java
+++ src/main/java/io/github/fishstiz/packed_packs/compat/resourcify/ResourcifyButtons.java
@@ -4,7 +4,6 @@ import dev.dediamondpro.resourcify.gui.injections.PackScreensAddition;
 import dev.dediamondpro.resourcify.services.ProjectType;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.packs.PackSelectionScreen;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentContents;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import org.jetbrains.annotations.Nullable;
@@ -15,8 +14,8 @@ public class ResourcifyButtons {
     private ResourcifyButtons() {
     }
 
-    public static @Nullable List<? extends Button> getButtons(PackSelectionScreen packScreen, Component title) {
-        ComponentContents contents = title.getContents();
+    public static @Nullable List<? extends Button> getButtons(PackSelectionScreen packScreen) {
+        ComponentContents contents = packScreen.getTitle().getContents();
 
         if (contents instanceof TranslatableContents translatable) {
             ProjectType type = PackScreensAddition.INSTANCE.getType(translatable.getKey());
