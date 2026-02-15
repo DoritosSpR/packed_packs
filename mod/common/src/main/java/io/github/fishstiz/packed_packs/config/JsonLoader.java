5d669b70f4eb87bb3e9a05d3e3c30dbdef288de5
fishstiz <fishstickz7777@gmail.com>
2026-02-15 23:10:51 +0800

    remove config fixer (v1 to v2)

--- mod/common/src/main/java/io/github/fishstiz/packed_packs/config/JsonLoader.java
+++ mod/common/src/main/java/io/github/fishstiz/packed_packs/config/JsonLoader.java
@@ -16,7 +16,6 @@ public class JsonLoader {
     private static final Gson GSON = new GsonBuilder()
             .setPrettyPrinting()
             .registerTypeAdapter(PackOverride.class, new PackOverride.Adapter())
-            .addSerializationExclusionStrategy(new ConfigFixer.ProfileExclusionStrategy())
             .create();
 
     private JsonLoader() {
